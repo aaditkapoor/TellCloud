131f1c0e120f85e7e543f467e012e917de2036ad
Aadit Kapoor <aadit@aadit.(none)>
2014-06-11 23:27:24 +0530

    First

---
+++ Information.py
@@ -0,0 +1,12 @@
+'''
+	****** TellCloud.com ******
+	A simple website that gives us information about Cloud Computing
+	Programmer: Aadit Kapoor ( Server side coding )
+	Designer: Template Downloaded
+	Version: 1.0.0.0
+	Written in: Python\Django	
+	
+	Email: aadit.kapoor@outlook.com	
+	Facebook: www.facebook.com/aadit.kapoor71
+	Github: Username: akapoorx00
+'''
